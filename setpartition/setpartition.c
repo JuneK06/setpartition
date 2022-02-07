4ef97a3f298bb62d64fcc2e3b41d41fe6edf7e66
JuneK06 <ka0wa6ju1n9-en314@ezweb.ne.jp>
2022-02-07 13:48:24 +0900

    初期

---
+++ setpartition/setpartition.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+
+#define N 3
+
+void bsort(int* x)
+{
+	int i, j;
+
+	for (i = 0; i < N - 1; i++)
+		for (j = N - 1; j > i; j--)
+			if (x[j - 1] > x[j]) {
+				int temp = x[j];
+				x[j] = x[j - 1];
+				x[j - 1] = temp;
+			}
+}
+
+int main(void)
+{
+	int x[N];
+	int a = 0, b = 0;
+	int i, j;
+	int trgt = 0, best;
+
+	for (i = 0; i < N; i++) {
+		x[i] = rand();
+		trgt += x[i];
+	}
+	trgt /= 2;
+
+	for (i = 1; i < N; i++) {
+		for (j = 0; j < i; j++) {
+			a += x[j];
+		}
+	}
+
+	return 0;
+}
