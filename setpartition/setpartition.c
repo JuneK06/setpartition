ac72c659f5d86a5f8e10cb1dcdf6ce2d5d7124d1
JuneK06 <ka0wa6ju1n9-en314@ezweb.ne.jp>
2022-02-10 12:57:42 +0900

    総当たり作成

--- setpartition/setpartition.c
+++ setpartition/setpartition.c
@@ -1,38 +1,43 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 
-#define N 3
+#define N 6
 
-void bsort(int* x)
+int trgt = 0;
+int best = 10000;
+int x[N];
+
+void setpar(int a, int tmp, int cnt)
 {
-	int i, j;
-
-	for (i = 0; i < N - 1; i++)
-		for (j = N - 1; j > i; j--)
-			if (x[j - 1] > x[j]) {
-				int temp = x[j];
-				x[j] = x[j - 1];
-				x[j - 1] = temp;
-			}
+	int i;
+	for (i = 0; i < N - a + 1; i++) {
+		int c = tmp + x[i + cnt];
+		if (cnt + 1 < a) {
+			setpar(a, c, cnt + 1);
+		}
+		else {
+			if (best > (trgt > c) ? trgt - c : c - trgt)
+				best = c;
+		}
+	}
 }
 
 int main(void)
 {
-	int x[N];
-	int a = 0, b = 0;
-	int i, j;
-	int trgt = 0, best;
+	int i;
 
 	for (i = 0; i < N; i++) {
-		x[i] = rand();
+		x[i] = rand() % 100;
 		trgt += x[i];
+		printf("%d ", x[i]);
 	}
 	trgt /= 2;
+	printf("\n目標値：%d\n", trgt);
 
-	for (i = 1; i < N; i++) {
-		for (j = 0; j < i; j++) {
-			a += x[j];
-		}
-	}
+	for (i = 1; i <= N / 2; i++)
+		setpar(i, 0, 0);
+
+	printf("%d", best);
 
 	return 0;
 }
